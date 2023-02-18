fdc1d8d0a9f19779ebdb8b17aed7909a8967f3ba
iamsammys <samuel.ezeh.246233@unn.edu.ng>
2023-02-18 11:17:28 +0100

    rm all

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-for a in range(97, 121):
-    print("{:c}".format(a), end='')
--- 0x01-python-if_else_loops_functions/test_files/print_alpha.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-for i in range(97, 123):
-    if i < 122:
-        print(f"{i:c}, ", end='')
-    else:
-        print(f"{i:c}")
