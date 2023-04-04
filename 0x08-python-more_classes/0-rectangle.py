61a3721094de9bcfa7f5beffc8ac33dd333c91ab
iamsammys <samuel.ezeh.246233@unn.edu.ng>
2023-04-04 02:21:19 +0100

    Rectangle class instantiation

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Module for the implementation of a class
+"""
+
+
+class Rectangle:
+    """Class Rectangle
+    """
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+"""Module for the implementation of Rectangle
+"""
+
+
+class Rectangle:
+    """Class Rectangle
+    """
+    def __init__(self, width=0, height=0):
+        """Initializes the class objects
+
+        Args:
+            width (int): width of the Rectangle
+            height (int): height of Rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Gets the value of width
+        
+        Returns:
+            int: width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the value of width
+
+        Args:
+            value (int): the value to assign to width
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Gets the height
+        
+        Returns:
+            int: height of Rectangle
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets the height to value
+
+        Args:
+            value (int): The value assigned to height
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
