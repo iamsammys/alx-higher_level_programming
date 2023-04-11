e86acf5d369fcd40dd584a4ce9d158e3ee4ae997
iamsammys <samuel.ezeh.246233@unn.edu.ng>
2023-04-11 18:11:20 +0100

    Same class or inherit from

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""module to implement isinstace
+"""
+
+
+def is_same_class(obj, a_class):
+    """checks whether an object is instance of a class
+    """
+    return (isinstance(obj, a_class) and type(obj) is a_class)
