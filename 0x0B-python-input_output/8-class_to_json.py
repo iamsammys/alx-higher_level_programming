597f483c1f3469354250eb5c8cec1b15326bb29f
iamsammys <samuel.ezeh.246233@unn.edu.ng>
2023-04-14 11:38:58 +0100

    Class to JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""module to create a dictionary description
+of an instance for json serialisation
+"""
+
+
+def class_to_json(obj):
+    """returns the dictionary description
+    with simple data structure
+
+    Returns:
+        dictionary for json serialization
+    """
+    return {key: value for key, value in obj.__dict__.items()}
+        
