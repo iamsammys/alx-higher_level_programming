4716d7bfeffaf15a2c79497bc25e54221552e4c9
iamsammys <samuel.ezeh.246233@unn.edu.ng>
2023-04-14 15:41:05 +0100

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+"""module to implement a class Student
+"""
+
+
+class Student:
+    """a class Student
+    """
+    def __init__(self, first_name, last_name, age):
+        """initialises the instances
+
+        Args:
+            first_name (str): the first name of Student
+            last_name (str): last name of Student
+            age (int): age of Student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves the dictionary representation of Student
+
+        Returns:
+            dictionary representation of the class
+        """
+        if attrs is None:
+            return self.__dict__
+        return {key: value for key, value in self.__dict__.items()
+                if key in attrs}
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance
+
+        Args:
+            json (file): file to load data from
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
