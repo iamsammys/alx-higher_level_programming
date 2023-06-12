ecb06027d0da8f63643f5b0ab2a8d4bb73baf6d0
iamsammys <samuel.ezeh.246233@unn.edu.ng>
2023-06-12 18:10:35 +0100

    sjlkjdlksd

---
+++ Javascript/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+const content = ["C is fun", "Python is cool", "Javascript is amazing"];
+content.forEach(value => {
+	console.log(value)
+});
---
+++ Javascript/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+const lang = ["C is fun", "Python is cool", "Javascript is amazing"];
+lang.forEach(lang => {
+	console.log(lang);
+});
