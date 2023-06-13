49f5ffd4d8994a5268c9f166d0ac4eaa6f6d5fda
iamsammys <samuel.ezeh.246233@unn.edu.ng>
2023-06-13 15:46:51 +0100

    fixed a code

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,15 @@
 #!/usr/bin/node
 
 const factorial = number => {
-  if (number > 0) { return (factorial(number - 1) * number); } else { return (1); }
+  if (number > 0) {
+    return (factorial(number - 1) * number);
+  } else {
+    return (1);
+  }
 };
 const num = parseInt(process.argv[2]);
-if (num) { console.log(factorial(num)); } else { console.log(1); }
+if (num) {
+  console.log(factorial(num));
+} else {
+  console.log(1);
+}
