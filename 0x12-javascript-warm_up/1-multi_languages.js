4b46335c14c34d73a9559d17fb8c95a9a176ee59
iamsammys <samuel.ezeh.246233@unn.edu.ng>
2023-06-13 15:27:52 +0100

    modified a file

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const content = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const content = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 content.forEach((value) => {
   console.log(value);
 }
