e5708c655878dfb804ab75c9bd81058e9a4929ac
iamsammys <samuel.ezeh.246233@unn.edu.ng>
2023-06-15 16:28:39 +0100

    modification

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -4,7 +4,7 @@ exports.nbOccurences = function (list, searchElement) {
   for (let i = 0; i < list.length; i++) {
     if (searchElement === list[i]) {
       count++;
-  }
+    }
   }
   return (count);
-}
+};
