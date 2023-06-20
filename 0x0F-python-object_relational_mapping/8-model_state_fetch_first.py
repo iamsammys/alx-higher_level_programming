92917335d9c058743a2bbe3439a0fcf3855f4b1f
iamsammys <samuel.ezeh.246233@unn.edu.ng>
2023-06-20 17:53:57 +0100

    Python sqlalchemy ORM

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
 """
-A script that prints the first State object from the database
+A script that lists all State objects from the database
 """
-
-from model_state import Base, State
-from sqlalchemy import (create_engine)
+from model_state import State, Base
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sys import argv
+import sys
+
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(argv[1], argv[2], argv[3]))
-    Base.metadata.create_all(engine)
+    passwd = sys.argv[2]
+    user = sys.argv[1]
+    db_name = sys.argv[3]
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:\
+                           3306/{}".format(user, passwd, db_name))
+
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).order_by(State.id).first()
-    if state is not None:
-        print("{}: {}".format(state.id, state.name))
-    else:
-        print("Nothing")
-    session.close()
+    name, id = session.query(State.name, State.id).order_by(State.id).first()
+    print("{:d}: {:s}".format(id, name))
