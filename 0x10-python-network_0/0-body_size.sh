d5f14c19626d2bffacea2f9d5a2b81cb88b0217a
iamsammys <samuel.ezeh.246233@unn.edu.ng>
2023-06-30 14:17:04 +0100

    curly command

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # bash script that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
-url=$1
-curl -Is url | grep Content-Length | cut -f2 -d " "
+curl -Is $1 | grep Content-Length | cut -f2 -d " "
