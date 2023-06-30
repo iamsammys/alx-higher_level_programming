f959ed360964c4c3b1e6c4667cff55ee028c10cb
iamsammys <samuel.ezeh.246233@unn.edu.ng>
2023-06-30 14:06:08 +0100

    curl command

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# bash script that takes in a URL, sends a request to that URL,
+# and displays the size of the body of the response
+url=$1
+curl -Is url | grep Content-Length | cut -f2 -d " "
