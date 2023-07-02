75c8492345eea53748cb6776cf42db295ce3f1f9
iamsammys <samuel.ezeh.246233@unn.edu.ng>
2023-07-02 18:33:45 +0100

    cURL a JSON file

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # bash script that sends a JSON POST request to a URL passed as the first argument
-curl -s -d "@$2" -H "Content-Type:application/json" -X POST $1
+curl -s -d "@$2" -H "Content-Type: application/json" -X POST $1
