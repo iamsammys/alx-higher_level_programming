411c708c20096a0263d542e17b2b6c65a2b31e2a
iamsammys <samuel.ezeh.246233@unn.edu.ng>
2023-07-02 17:06:30 +0100

    Find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def find_peak(list_of_integers):
+    """function that finds a peak in a list of unsorted integers
+
+    Args:
+        list_of_integers (list): a list of integers
+
+    Returns:
+        the peak number
+    """
+    if len(list_of_integers) <= 0:
+        return None
+    l, r = 0, len(list_of_integers) - 1
+    while l < r:
+        mid = (r + l) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            l = mid + 1
+        else:
+            r = mid
+    return list_of_integers[l]
